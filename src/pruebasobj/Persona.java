8f3a59de319fb6ee9b2660e93c73f82fc48933bf
Mario <mario.laso@iesnervion.es>
2026-01-12 11:57:27 +0100

    Update genero field in Persona class

--- src/pruebasobj/Persona.java
+++ src/pruebasobj/Persona.java
@@ -4,7 +4,7 @@ public class Persona {
 		String nombre = "Sin nombre";
 		int edad; // Por defecto, los numeros se inicializan en 0
 		double estatura = 0.30;
-		String genero = "Helicoptero apache transexual"; 
+		String genero = "Helicoptero apache"; 
 		boolean carnet = false;
 		final String DNI = "No hay DNI";
 	}
